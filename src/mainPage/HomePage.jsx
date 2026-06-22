2afac7aa5531be58a533eb8bcaf21c4d1dc2b802
abdulahad95460 <abdulahad95460@gmail.com>
2026-06-22 20:04:01 +0530

    alert included after clicking on the button

--- src/mainPage/HomePage.jsx
+++ src/mainPage/HomePage.jsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Questions from '../Question'
 
 
-
 const HomePage = () => {
+
     const [homeDisplay, sethomeDisplay] = useState(false)
+    useEffect(() => {
+  if (homeDisplay) {
+    alert("Hey User, as you tapped the play button , remem ber there's a stopwatch to track how much time will it take you to complete that game ");
+  }
+}, [homeDisplay]);
 
     if(homeDisplay){
         return <Questions />
     }
   return (
     <div className='homePageDiv'>
+      
         <h1 className='homePageHeader'>Let's Start</h1>
         <button className='homePageButton' onClick={()=> sethomeDisplay(true)}>Click To Play</button>
     </div>
